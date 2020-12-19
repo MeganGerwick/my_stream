6aade07898c4bc9b08da85891a56c2146e370b14
Christian Mills <millsy4@hotmail.com>
2020-12-19 13:27:32 -0600

    added urls to use for ajax

---
+++ public/js/moviedb.js
@@ -0,0 +1,21 @@
+let API_KEY = "e84c7c8e45cf9ee6ec79d9c662ff6222";
+
+//Search for a specific movie https://api.themoviedb.org/3/search/movie?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US&query=Harry%20Potter%20and%20the%20Sorcerer%27sStone
+
+//Search for a specific actor https://api.themoviedb.org/3/search/person?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US&query=Daniel%20Radcliffe
+
+//Search for a specific TV show https://api.themoviedb.org/3/search/tv?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US&query=The%20Good%20Place
+
+//Get the official list of genres for all movies https://api.themoviedb.org/3/genre/movie/list?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US
+
+//Get the official list of genres for all tv shows https://api.themoviedb.org/3/genre/tv/list?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US
+
+//Search for similar movies https://api.themoviedb.org/3/movie/671/similar?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US  <-- WILL NEED ID OF THE MOVIE
+
+//Trending movies https://api.themoviedb.org/3/trending/movie/day?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222
+
+//Trending TV shows https://api.themoviedb.org/3/trending/tv/day?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222
+
+//Get services of "Where to Watch" https://api.themoviedb.org/3/movie/550/watch/providers?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222
+
+//Get movies in theatres now https://api.themoviedb.org/3/movie/now_playing?api_key=e84c7c8e45cf9ee6ec79d9c662ff6222&language=en-US&region=US
