fb653d4dfd070bfcc7adda335d297b0bceaa3a72
Louisa May <mgerwick@gmail.com>
2020-12-22 19:24:29 -0600

    set up nodemailer

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,5 @@
 const $ = window.$;
+const nodemailer = require("nodemailer");
 
 $(document).ready(() => {
   // Getting references to our form and input
@@ -25,19 +26,57 @@ $(document).ready(() => {
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser (email, password) {
+  function signUpUser(email, password) {
     $.post('/api/signup', {
       email: email,
       password: password
     })
       .then(() => {
+        //add nodemailer code here
+        async function main() {
+
+          // create reusable transporter object using the default SMTP transport
+          let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: process.env.node_mailer_email, // generated ethereal user
+              pass: process.env.node_mailer_password, // generated ethereal password
+            },
+          });
+
+          //verify connection configuration
+          transporter.verify(function (error, success) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Server is ready to take our messages");
+            }
+          });
+
+          // send mail with defined transport object
+          let info = await transporter.sendMail({
+            from: '"My Stream 👻" <mystreamwebsite@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Welcome to My Stream", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+          });
+          console.log("Message sent: %s", info.messageId);
+
+
+        };
+
+        //sends user to their member homepage 
         window.location.replace('/members');
+
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
   }
 
-  function handleLoginErr (err) {
+  function handleLoginErr(err) {
     $('#alert .msg').text(err.responseJSON);
     $('#alert').fadeIn(500);
   }
