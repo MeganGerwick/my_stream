6abece2e97f022dd4181aad543e1efe6f96be1c2
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-05 20:53:23 -0600

    working on hbs route

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,49 +1,55 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require('path');
+const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require('../../config/middleware/isAuthenticated');
-const router = require('express').Router();
+const isAuthenticated = require("../../config/middleware/isAuthenticated");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // If the user already has an account send them to the members page
   if (req.user) {
-    res.redirect('/members');
+    res.redirect("/members");
   }
 
-  res.sendFile(path.join(__dirname, '../../public/signup.html'));
+  res.sendFile(path.join(__dirname, "../../public/signup.html"));
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user already has an account send them to the members page
   if (req.user) {
-    res.redirect('/members');
+    res.redirect("/members");
   }
 
-  res.sendFile(path.join(__dirname, '../../public/login.html'));
+  res.sendFile(path.join(__dirname, "../../public/login.html"));
 });
 
 // Route for logging user out
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
-router.get('/members', isAuthenticated, (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/members.html'));
+router.get("/members", isAuthenticated, (_req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/members.html"));
 });
 
-router.get('/search', isAuthenticated, (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/search.html'));
-})
+router.get("/search", isAuthenticated, (_req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/search.html"));
+});
 
-router.get('/watchlist', isAuthenticated, (_req, res) => {
+router.get("/watchlist", isAuthenticated, (req, res) => {
+  watchlist.all(function(data) {
+    const list = {
+      watchlist: data,
+    };
+    console.log(list);
+    res.render("index", list);
+  });
+
   // 1. what user called and their id
   // 2. call database
   // 3. pass into 2nd arg of res.render
-  res.render("index", {});
-})
-
+});
 
 module.exports = router;
