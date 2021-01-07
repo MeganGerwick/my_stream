8e24cf9d646282b1d3963457b1842db7f0927fef
Louisa May <mgerwick@gmail.com>
2021-01-07 13:53:28 -0600

    added session secret

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "my_stream",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "SESSION_SECRET",
+    database: "my_stream",
 });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
